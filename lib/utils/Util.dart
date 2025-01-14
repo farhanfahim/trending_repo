b8c6bac00e15f643f25af5832f8025892f21e950
farhanfahim <dev.farhanfahim@gmail.com>
2025-01-14 13:39:27 +0500

    api added

--- lib/utils/Util.dart
+++ lib/utils/Util.dart
@@ -4,6 +4,16 @@ import '../app/components/widgets/custom_toast.dart';
 
 class Util {
 
+  static Future<bool> check() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.mobile) {
+      return true;
+    } else if (connectivityResult == ConnectivityResult.wifi) {
+      return true;
+    }
+    return false;
+  }
+
   static void hideKeyBoard(BuildContext context) {
     FocusScope.of(context).requestFocus(FocusNode());
   }
